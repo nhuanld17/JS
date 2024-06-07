666db70338fa2c57353885b62eb70fc728f934ad
Lê Đình Nhuận <teo@.gmail.com>
2024-06-07 08:39:03 +0700

    Practice with Fetch API

--- Bai_22/fetchAPI.js
+++ Bai_22/fetchAPI.js
@@ -13,9 +13,24 @@
         .catch((error) => {
             console.log(error);
         });
- */
+*/
 
+// Hàm để dùng chung cho GET CATEGORY và GET PRODUCT
+const fetchAPI = (url) => {
+    const result = fetch(url)
+        .then((respone) => {
+        return respone.json();
+        })
+        .then((data) => {
+        console.log(data);
+        return data;
+        });
+    return result;
+};
+
+/* 
     // Ví dụ
+    // GET CATEGORY
     fetch("https://dummyjson.com/products/category-list") // Gửi yêu cầu fetch
     .then((respone) => {
         return respone.json(); // Xử lí đối tượng respone, chuyển dữ liệu JSON được trả về thành 1 đối tượng JavaScript
@@ -30,3 +45,55 @@
         console.log(html); // In dữ liệu
         document.querySelector("#category").innerHTML = html;
     });
+
+    // GET PRODUCT
+    fetch("https://dummyjson.com/products")
+        .then((respone) => {
+            return respone.json();
+        })
+        .then((data) => {
+            console.log(data.products);
+            let htmls = "";
+            data.products.forEach((element) => {
+                htmls += `
+                    <div class="product-item">
+                        <img src="${element.thumbnail}" alt="${element.title}">
+                        <h3>${element.title}</h3>
+                        <p>${element.price}</p>
+                    </div> 
+                `;
+            });
+
+            document.querySelector("#products").innerHTML = htmls;
+        })
+ */
+
+        // Sử dụng hàm fetchAPI ở trên áp dụng vào GET CATEGORY và GET PRODUCT
+        fetchAPI("https://dummyjson.com/products/category-list") // Trả về đối tượng JS
+        .then((data) => {
+            let html = "";
+            data.forEach((element) => {
+            html += `
+                    <div class="category-item">${element}</div>
+                    `;
+            });
+            console.log(html); // In dữ liệu
+            document.querySelector("#category").innerHTML = html;
+        });
+
+        fetchAPI("https://dummyjson.com/products") // Trả về đối tượng JS
+        .then((data) => {
+            let htmls = "";
+            data.products.forEach((element) => {
+                htmls += `
+                    <div class="product-item">
+                        <img src="${element.thumbnail}" alt="${element.title}">
+                        <h3>${element.title}</h3>
+                        <p>${element.price}</p>
+                    </div> 
+                `;
+            });
+            document.querySelector("#products").innerHTML = htmls;
+        });
+
+
