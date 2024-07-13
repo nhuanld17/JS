3731ac8a635eae9e776d2451492254bbec8cd478
Le Dinh Nhuan <ledinhnhuan1917@gmail.com>
2024-07-13 22:54:56 +0700

    Review Bai 15

--- Bai_15/script.js
+++ Bai_15/script.js
@@ -1,4 +1,8 @@
-var button = document.querySelector("button");
-button.onclick = function(){
-    alert("Hello World");
+// Tìm và tham chiếu đến phần tử button đầu tiên trong tài liệu html
+var button = document.querySelector("button"); 
+
+// Gán 1 hàm sử lí sự kiện onclick cho button
+button.onclick = function () { 
+    // Khi button được nhấn, hiển thị một thông báo với nội dung hello world
+    alert("Hello world");
 }
